c113543ca4fd0d548e4a3bc6d1e8bc2edce1ca21
BiancaBrandao <biancabrandao199@gmail.com>
2022-04-06 18:00:39 +0000

    me salva senhor

---
+++ OutraQuestao.java
@@ -0,0 +1,13 @@
+public class OutraQuestao {
+    public static void main(String[] args) {
+        int sequencia[] = new int[10];
+        razao = 5;
+        inicio = 125;
+
+        for(int i = 125;inicio < 10;indice++)) {
+            inicio[razao] = inicio*razao;
+        }
+
+        System.out.println(" ");
+    }
+}
